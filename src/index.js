d63d4d0442b37de80481f0b2d4bd5399786ffb94
Victory <raccooccon@gmail.com>
2019-10-03 14:38:52 +0300

    Add solution

--- src/index.js
+++ src/index.js
@@ -4,7 +4,28 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let string = expr.replace(/\s/g, '');
+    let array = string.split('');
+    let indexLeftBr = [];
+    let indexRightBr = [];
+    let filtered = array.filter((elem, i) => {
+      if (elem == "(") {
+        indexLeftBr.push(i);
+      }
+      if (elem == ")") {
+        indexRightBr.push(i);
+      }
+    });
+
+    if (indexLeftBr.length != indexRightBr.length) {
+        throw new Error("ExpressionError: Brackets must be paired");
+    }
+
+    if (string.search(/\/0/) != -1) {
+        throw new Error("TypeError: Division by zero.");  
+    }
+
+    return new Function('return ' + expr)();
 }
 
 module.exports = {
